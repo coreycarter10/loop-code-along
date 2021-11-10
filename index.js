9efe1c444ddfb95b4d29a0bad768a3ae56b370b5
Corey Carter <corey-carter@hotmail.com>
2021-11-10 12:46:38 -0700

    Added loop functionality

---
+++ index.js
@@ -0,0 +1,33 @@
+// console.log("Everything is running");
+
+let jonSnowAttack = 35;
+let jameiLannisterAttack = 35;
+
+let jonSnowHealth = 50;
+let jonSnowDefense = 0;
+
+let jamieLannisterHealth = 50;
+let jamieLannisterDefense = 20;
+
+// if (jonSnowAttack < jameiLannisterAttack) {
+//     console.log("Jamie has the stronger attack");
+// } else if (jonSnowAttack > jameiLannisterAttack) {
+//     console.log('Jon has the stronger attack');
+// } else {
+//     console.log('The attacks are equally powerful');
+// }
+
+jonSnowDefense += 25;
+
+// for (let i = 0; i < 3; i++) {
+//     if (jonSnowHealth <= 0) {
+//         console.log('Jon Snow is toast');
+//     } else {
+//         jonSnowHealth -= (jameiLannisterAttack - jonSnowDefense);
+//         console.log(`Jon's health is ${jonSnowHealth}`);
+//     }
+// }
+
+for (let i = 10; i > 0; i--) {
+    console.log(i);
+}
